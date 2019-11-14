40fd92abb43adb3e2fb777a817bb9ec39bc50ef7
Andrew <andrewriznykua93@gmail.com>
2019-11-14 09:24:48 -0600

    aaaaa

---
+++ app/controllers/puzzles_controller.rb
@@ -0,0 +1,12 @@
+class PuzzlesController <ApplicationController
+
+  def index
+    puzzles = Puzzle.all
+    render json: puzzles
+  end
+
+  def show
+    puzzle = Puzzle.find_by(id: params[:id])
+    render json: puzzle
+  end
+end
